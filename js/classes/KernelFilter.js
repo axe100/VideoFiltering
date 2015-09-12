729354db9c414ec5e56c395db22b9319fb0ff778
Alexander Ries <alexander.ries@sap.com>
2015-09-13 00:32:49 +0200

    video frame get now filtered with sample black-white filter an get displayed in canvas

---
+++ js/classes/KernelFilter.js
@@ -0,0 +1,20 @@
+/**
+ * Created by D062114 on 9/12/15.
+ */
+
+function KernelFilter() {
+
+    this.applyFilterToFrameDataWithMatrix = function (frameData, matrix) {
+        var d = frameData.data;
+        for (var i=0; i<d.length; i+=4) {
+            var r = d[i];
+            var g = d[i+1];
+            var b = d[i+2];
+            // CIE luminance for the RGB
+            // The human eye is bad at seeing red and blue, so we de-emphasize them.
+            var v = 0.2126*r + 0.7152*g + 0.0722*b;
+            d[i] = d[i+1] = d[i+2] = v
+        }
+        return frameData;
+    }
+}
--- js/main.js
+++ js/main.js
@@ -2,17 +2,60 @@
  * Created by D062114 on 9/12/15.
  */
 
-var kernelInputFields = new KernelInputFields();
+var KernelInputFields = new KernelInputFields();
+var NameLater = new NameLater();
+var KernelFilter = new KernelFilter();
+var Canvas = new Canvas();
+
+var video;
+var videoWith;
+var videoHeight;
+var drawingCanvas;
+var drawingContext;
+var dataRetrivalHelperCanvas;
+var dataRetrivalHelperContext;
 
 function init() {
+    initVariables();
     addFileInputChangeListener();
-    kernelInputFields.generateKernelInputFields();
+    addVideoPlayEventListener();
+    KernelInputFields.generateKernelInputFields();
+}
+
+function initVariables() {
+    video = document.querySelector('video');
+    drawingCanvas = document.getElementById('canvas');
+    drawingContext = drawingCanvas.getContext('2d');
+    dataRetrivalHelperCanvas = document.createElement('canvas');
+    dataRetrivalHelperContext = dataRetrivalHelperCanvas.getContext('2d');
+}
+
+function draw() {
+    if(video.paused || video.ended)	return false;
+    dataRetrivalHelperContext.drawImage(video,0,0,videoWith, videoHeight);
+    var frameData = dataRetrivalHelperContext.getImageData(0, 0, videoWith, videoHeight);
+    var matrix;
+    frameData = KernelFilter.applyFilterToFrameDataWithMatrix(frameData, matrix);
+    drawingContext.putImageData(frameData, 0, 0);
+    setTimeout(draw, 20);
 }
 
 function addFileInputChangeListener() {
     var inputNode;
     inputNode = document.querySelector('input');
     inputNode.addEventListener('change', playSelectedFile, false);
+}
+
+function addVideoPlayEventListener() {
+    video.addEventListener('play', function(){
+        videoWith = video.clientWidth;
+        videoHeight = video.clientHeight;
+        drawingCanvas.width = videoWith;
+        drawingCanvas.height = videoHeight;
+        dataRetrivalHelperCanvas.width = videoWith;
+        dataRetrivalHelperCanvas.height = videoHeight;
+        draw();
+    },false);
 }
 
 function playSelectedFile(event) {
