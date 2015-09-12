064a7b16e6d4aa01a0ee3ef1159f1c0950c6de94
Alexander Ries <alexander.ries@sap.com>
2015-09-12 18:35:28 +0200

    added video and file input + video playback

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
 /**
  * Created by D062114 on 9/12/15.
  */
+
+function init() {
+    addFileInputChangeListener();
+}
+
+function addFileInputChangeListener() {
+    var inputNode;
+    inputNode = document.querySelector('input');
+    inputNode.addEventListener('change', playSelectedFile, false);
+}
+
+function playSelectedFile(event) {
+    var file = this.files[0];
+    var type = file.type;
+    var videoNode = document.querySelector('video');
+    var canPlay = videoNode.canPlayType(type);
+    if (canPlay == false) {
+        return;
+    }
+    var fileURL = URL.createObjectURL(file);
+    videoNode.src = fileURL;
+}
