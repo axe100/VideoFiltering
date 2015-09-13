330f523e51ff8dfc300cc1c65e8a60adf1e7f773
Alexander Ries <alexander.ries@sap.com>
2015-09-13 02:40:03 +0200

    deleted test filter method from KernelFilter class

--- js/classes/KernelFilter.js
+++ js/classes/KernelFilter.js
@@ -4,20 +4,6 @@
 
 function KernelFilter() {
 
-    this.applyFilterToFrameDataWithMatrix = function (frameData, matrix) {
-        var d = frameData.data;
-        for (var i=0; i<d.length; i+=4) {
-            var r = d[i];
-            var g = d[i+1];
-            var b = d[i+2];
-            // CIE luminance for the RGB
-            // The human eye is bad at seeing red and blue, so we de-emphasize them.
-            var v = 0.2126*r + 0.7152*g + 0.0722*b;
-            d[i] = d[i+1] = d[i+2] = v
-        }
-        return frameData;
-    }
-
     this.applyKernelFilterToFrameDataWithMatrix = function(frame, weights, opaque) {
         var kernelWidth = Math.round(Math.sqrt(weights.length));
         var kernelBorderWidth = Math.floor(kernelWidth/2);
